8524f658bd8034bd3c339a935db518c8f943f3e3
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2022-08-06 12:02:58 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// localStorage
 window.addEventListener("load", () => {
   todos = JSON.parse(localStorage.getItem("todos")) || [];
   const nameInput = document.querySelector("#name");
@@ -7,18 +6,10 @@ window.addEventListener("load", () => {
   nameInput.addEventListener("change", (e) => {
     localStorage.setItem("username", e.target.value);
   });
-
-  overlayForm.addEventListener("submit", (e) => {
-    const todo = {
-      content: overlayInputValue,
-      done: false,
-    };
-    todos.push(todo);
-    (localStorage.setItem = "todos"), JSON.stringify("todos");
-    e.target.reset();
-  });
 });
 // end localStorage declaration
+<<<<<<< HEAD
+=======
 // var keyInput = document.querySelector("#overlay-form-input");
 // var overlayForm = document.querySelector("#overlay-form");
 // document.addEventListener("keydown", function keypressed(e) {
@@ -42,24 +33,29 @@ window.addEventListener("load", () => {
 
 //   console.log(e);
 // });
+>>>>>>> a2c40153c870ddd975a139278e6905f153bb98ec
 
 // functional add button which shows overlay form onclick
 var addBtn = document.querySelector("#addBtn");
 var overlayForm = document.querySelector("#overlay-form");
 var container = document.querySelector(".container");
 var header = document.querySelector("header");
+var keyInput = document.querySelector("#overlay-form-input");
+
 addBtn.addEventListener("click", () => {
   if (overlayForm.classList.contains("hide")) {
     overlayForm.classList.remove("hide");
     overlayForm.classList.add("flex");
     container.style.opacity = "20%";
     var overlayInputValue = document.querySelector("#overlay-form-input").value;
-    overlayInputValue = "";
-    console.log(overlayInputValue);
+    overlayInput.focus();
+    console.log(`*Adding New task*`);
+    overlayInput.value = "";
   }
 });
-// end
+// add button end
 
+// const num = () => {};
 // cancel button which hides overlay form onclick
 var cancel = document.querySelector(".clear");
 const clr = () => {
@@ -68,6 +64,7 @@ const clr = () => {
     overlayForm.classList.remove("flex");
     container.style.opacity = "100%";
     invalid.textContent = "";
+    console.log('Cancelled addition');
   }
 };
 cancel.addEventListener("click", clr);
@@ -79,53 +76,69 @@ var save = document.querySelector("#submit");
 var overlayForm = document.querySelector("#overlay-form");
 var invalid = document.querySelector("#invalid");
 var ul = document.querySelector(".tasks");
-// var list = document.createElement("li");
 var taskCount = 0;
 var watermark = document.querySelector("#watermark");
+
+// #watermark to be shown
 const showWatermark = () => {
-  if (watermark.classList.contains("flex") ) {
+  if (watermark.classList.contains("flex")) {
     watermark.classList.remove("flex");
     watermark.classList.add("hide");
   }
-  // else if(taskCount)
-
 };
+// end of watermark function
+
 const saveTodo = () => {
   var overlayInputValue = document.querySelector("#overlay-form-input").value;
-
   if (overlayInputValue == "") {
     invalid.textContent = "No task added";
-  } else {
+    console.warn('Add a task to continue');
+  } 
+   else {
     if (!overlayForm.classList.contains("hide")) {
       overlayForm.classList.remove("flex");
       overlayForm.classList.add("hide");
       container.style.opacity = "100%";
     }
+
     var list = document.createElement("li");
     list.classList.add("list-item");
-    list.textContent = overlayInputValue;
     taskCount++;
-    var addlist=taskCount;
-     console.log(`addlist is ${addlist}`);
-  
-    // console.log(taskCount);
-      console.log(`the number of lists is ${taskCount}`);
+    var addlist = taskCount;
+    console.log(`You have ${addlist} lists now `);
+
+    var listText = document.createElement("p");
+    listText.setAttribute("id", "list-text");
+    listText.textContent = overlayInputValue;
+    list.appendChild(listText);
+
     var deleteBtn = document.createElement("button");
     deleteBtn.className = "trash";
+
     var binIcon = document.createElement("i");
     binIcon.classList.add("fa-solid", "fa-trash", "fa-2xl");
+    binIcon.setAttribute("accesskey", "d");
     deleteBtn.appendChild(binIcon);
     list.append(deleteBtn);
 
     ul.append(list);
-    console.log(overlayInputValue);
-    console.log(list);
-    overlayInput.value = "";
+
+    if (taskCount >= 10) {
+      ul.style.overflowY = "scroll";
+      console.log(`Will scroll now`);
+    }
+
     invalid.textContent = "";
-    
-    // var counter = 0;
-    // counter=list++;
-    // console.log(counter);
+
+    // const todos = [];
+    // for (let i = 0; i < addlist; i++) {
+    //   var listTextVal = document.querySelectorAll("#list-text").value;
+    //   const element = todos[i];
+    //   var newVal=overlayInput.value;
+    //   todos.push(overlayInput.value);
+    //   console.log(todos);
+    //   console.log(`listtextval is ${listText.textContent}`);
+    // }
   }
 };
 function onEnter() {
@@ -143,47 +156,57 @@ var task = document.querySelector(".tasks");
 var li = document.querySelector(".list-item");
 function removeItem(e) {
   if (e.target.classList.contains("fa-trash")) {
-    // console.log(1);
+    console.warn('Do you really wanna remove item? Tell alert');
 
     if (confirm("Are you sure?")) {
       var target = e.target.parentElement.parentElement;
       task.removeChild(target);
-      // console.log(target);
+      
       taskCount--;
-      var removelist=taskCount;
-      console.log(`removelist is ${removelist}`);
-      // console.log(taskCount);
-      console.log(`the number of lists is ${taskCount}`);
-      console.log(removelist);
+      var removelist = taskCount;
+      if (removelist < 1) {
+        watermark.classList.remove("hide");
+        watermark.classList.add("flex");
+        console.log('No list item watermark will show now');
+      }
+      console.log(`*Removing Lists*`);
+      console.log(`You have ${taskCount} lists now`);
     }
   }
 }
 ul.addEventListener("click", removeItem);
 
-// keyfunctions
+// this will run when keys are pressed
 const togglePopUp = () => {
   // check if overlay is hidden
   if (overlayForm.classList.contains("flex")) {
     overlayForm.classList.add("hide");
-    overlayForm.classList.remove('flex')}
-  // } else {
-  //   overlayForm.classList.add("hide");
-  //   //  overlayForm.classList.remove("flex");
-  // }
+    overlayForm.classList.remove("flex");
+  }
+
 };
 document.addEventListener("keydown", function (e) {
   if (e.key == "Escape") {
+    // togglePopUp checks if the overlayForm is hidden or not
     togglePopUp();
+    // makes the paragraph content zero
     invalid.textContent = "";
+<<<<<<< HEAD
+    keyInput.value = "";
+=======
 //     keyInput.value="";
 
+>>>>>>> a2c40153c870ddd975a139278e6905f153bb98ec
     container.style.opacity = "100%";
+     console.log("Cancelled addition");
   } else if (e.key === "Enter") {
     onEnter();
     showWatermark();
     container.style.opacity = "100%";
   }
- 
 });
+<<<<<<< HEAD
+=======
 
  
+>>>>>>> a2c40153c870ddd975a139278e6905f153bb98ec
